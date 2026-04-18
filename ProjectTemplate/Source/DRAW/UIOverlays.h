64703ffb2a27684518685f5f4e50bdc8b255985b
MTARedHammer <arringtonmyles142@gmail.com>
2026-04-17 22:50:37 -0400

    checking

--- ProjectTemplate/Source/DRAW/UIOverlays.h
+++ ProjectTemplate/Source/DRAW/UIOverlays.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "DrawComponents.h"
+#include "../GAME/GameComponents.h"
 #include "./Utility/FileIntoString.h"
 #include "shaderc/shaderc.h"
 #include "./Overlay.h"
@@ -20,16 +20,53 @@ void InitializeUIOverlays(entt::registry& registry, entt::entity entity) {
 	auto& font = registry.emplace<BLIT_Font>(entity, blitter, "../Source/DRAW/font.tga", font_Arial);
 }
 
-static void GameplayUI(Overlay& ovl, GW::GRAPHICS::GBlitter& bltr, BLIT_Font& font, int W, int H) {
+static void GameplayUI(entt::registry& registry, Overlay& ovl, GW::GRAPHICS::GBlitter& bltr, BLIT_Font& font, int W, int H) {
 	bltr.ClearColor(0x00000000);
 	font.DrawTextImmediate(W / 8, 25, "1UP", 3);
 	font.DrawTextImmediate(W - (W / 6), 25, "2UP", 3);
 	font.DrawTextImmediate((W / 2) - 100, 25, "HIGH SCORE", 10);
-	font.DrawTextImmediate(5, H - 20, "^^^^^^^", 7);
+	std::string hits;
+	auto player = registry.view<GAME::Player>();
+	for (auto entity : player) {
+		auto hitpoints = registry.get<GAME::Health>(entity).HP;
+		for (int i = 0; i < hitpoints; i++) {
+			hits[i] = '^';
+		}
+	}
+	font.DrawTextImmediate(5, H - 20, hits.c_str(), 7);
 	font.DrawTextImmediate(W - 75, H - 20, "RRR", 3);
 	unsigned int* pixels;
 	ovl.LockForUpdate(W * H, &pixels);
 	bltr.ExportResult(false, W, H, 0, 0, pixels, nullptr, nullptr);
 	ovl.Unlock();
 	ovl.TransferOverlay();
+}
+
+void UpdateUIOverlays(entt::registry& registry, int state, Overlay& ovl, GW::GRAPHICS::GBlitter& bltr, BLIT_Font& font, int W, int H) {
+	switch (state) {
+	case 0:
+
+	break;
+	case 1:
+		GameplayUI(registry, ovl, bltr, font, W, H);
+	break;
+	case 2:
+
+	break;
+	case 3:
+
+	break;
+	case 4:
+
+	break;
+	case 5:
+
+	break;
+	case 6:
+
+	break;
+	case 7:
+
+	break;
+	}
 }
